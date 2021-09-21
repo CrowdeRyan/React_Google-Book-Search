9415d74a20ab44baebad00ac391df59209178c37
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 14:10:21 -0400

    adding name

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,2 +1,10 @@
 import React from "react";
 import "./style.css";
+
+const Footer = () => (
+  <footer className="page-footer">
+    <div className="container-fluid text-center">
+      <p>Ryan Crowder</p>
+    </div>
+  </footer>
+);
