39f2f2eef08bd3645e127c7af058c9a5923fbaaf
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 14:24:09 -0400

    get books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,4 +5,9 @@ export default {
   getGoogleSearchBooks: function (query) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
   },
+
+  // Gets all books
+  getBooks: function () {
+    return axios.get("/api/books");
+  },
 };
