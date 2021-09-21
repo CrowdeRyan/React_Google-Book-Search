118905d51cfbbcfd8dfb973e5c296cfb2cd1b2e3
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 13:40:30 -0400

    setup rows

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -4,3 +4,7 @@ import React from "react";
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
+
+// This Row component lets us use a bootstrap row without having to think about class names
+export function Row({ fluid, children }) {
+    return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
