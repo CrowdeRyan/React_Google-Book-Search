05be2dc69f524712be3bca82bf4dbc6936cfbd14
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 13:56:20 -0400

    define result table

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,3 +1,28 @@
 import React from "react";
 import "./style.css";
 import { Row, Col } from "../Grid";
+
+const SearchResult = (props) => {
+    return props.books.length === 0 ? (
+      <div className="card">
+        <div className="card-body player">
+          <div className="article">
+            <h3>Search Results</h3>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="card">
+        <div className="card-body player">
+          <div className="article">
+            <h3>Search Results</h3>
+            {props.books.map((book) => {
+              return (
+            
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  };
