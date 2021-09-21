66c4dc5eb3fdf151a7ed9a639731af3a96dabae4
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 14:24:33 -0400

    get by id

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,4 +10,9 @@ export default {
   getBooks: function () {
     return axios.get("/api/books");
   },
+
+  // Gets the book with the given id
+  getBook: function (id) {
+    return axios.get("/api/books/" + id);
+  },
 };
