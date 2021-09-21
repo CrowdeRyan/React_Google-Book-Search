ea92ed4bc66e147eb6903b85475d73f56ed75e6b
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 18:56:50 -0400

    setup search component

---
+++ client/src/pages/SearchBooks.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron";
+import { Container, Row, Col } from "../components/Grid";
+import SearchForm from "../components/SearchForm";
+import SearchResult from "../components/SearchResult";
