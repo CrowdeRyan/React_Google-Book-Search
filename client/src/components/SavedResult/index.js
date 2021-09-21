7da9c92c411e990ce3d8dd33b447868e50afa5a8
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 14:04:00 -0400

    labeling table

--- client/src/components/SavedResult/index.js
+++ client/src/components/SavedResult/index.js
@@ -1,3 +1,29 @@
 import React from "react";
 import "./style.css";
 import { Row, Col } from "../Grid";
+
+
+const SavedResult = (props) => {
+    return props.savedBooks.length === 0 ? (
+      <div className="card">
+        <div className="card-body player">
+          <div className="article">
+            <h3>Your Saved Books</h3>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="card">
+        <div className="card-body player">
+          <div className="article">
+            <h3>Your Saved Books</h3>
+            {props.savedBooks.map((savedbook) => {
+              return (
+               
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  };
