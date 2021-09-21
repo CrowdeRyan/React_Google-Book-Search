fd66e416a23f78de862ec4110d9f0f94519ee915
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 19:44:08 -0400

    exporting

--- client/src/pages/SaveBooks.js
+++ client/src/pages/SaveBooks.js
@@ -8,11 +8,21 @@ class SaveBook extends Component {
   state = {
     savedBooks: [],
   };
+
+  //when this component mounts, grab all books that were save to the database
   componentDidMount() {
     API.getBooks()
       .then((res) => this.setState({ savedBooks: res.data }))
       .catch((err) => console.log(err));
   }
+
+  //function to remove book by id
+  handleDeleteButton = (id) => {
+    API.deleteBook(id)
+      .then((res) => this.componentDidMount())
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <Container fluid className="container">
@@ -27,3 +37,5 @@ class SaveBook extends Component {
     );
   }
 }
+
+export default SaveBook;
