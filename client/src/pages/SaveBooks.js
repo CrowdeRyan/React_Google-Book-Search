17162c027c46b1ce5d03c0976d5f21d4771d7785
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 19:19:11 -0400

    pulling state

--- client/src/pages/SaveBooks.js
+++ client/src/pages/SaveBooks.js
@@ -3,3 +3,9 @@ import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
 import { Container } from "../components/Grid";
 import SavedResult from "../components/SavedResult";
+
+class SaveBook extends Component {
+  state = {
+    savedBooks: [],
+  };
+}
