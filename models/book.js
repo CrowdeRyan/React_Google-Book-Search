121b9c30522eda8220445d61c3628672c670269c
Ryan Crowder <crowderyan@gmail.com>
2021-09-21 20:18:11 -0400

    detail schema

--- models/book.js
+++ models/book.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  description: String,
+  image: { type: String, trim: true },
+  link: { type: String, trim: true },
+  date: { type: Date, default: Date.now },
+});
